49cfb43447b3d3fe6abacf7bc243ed5fb60a2d5a
Will DeWind <will.dewind@gmail.com>
2017-04-12 13:21:09 -0400

    use net http to test if server is up instead of socket

--- lib/thin/controllers/cluster.rb
+++ lib/thin/controllers/cluster.rb
@@ -1,9 +1,9 @@
-require 'socket'
+require 'net/http'
 
 module Thin
   # An exception class to handle the event that server didn't start on time
   class RestartTimeout < RuntimeError; end
-  
+
   module Controllers
     # Control a set of servers.
     # * Generate start and stop commands and run them.
@@ -13,17 +13,17 @@ module Thin
       # Cluster only options that should not be passed in the command sent
       # to the indiviual servers.
       CLUSTER_OPTIONS = [:servers, :only, :onebyone, :wait]
-      
+
       # Maximum wait time for the server to be restarted
       DEFAULT_WAIT_TIME = 30    # seconds
-      
+
       # Create a new cluster of servers launched using +options+.
       def initialize(options)
         super
         # Cluster can only contain daemonized servers
         @options.merge!(:daemonize => true)
       end
-      
+
       def first_port; @options[:port]     end
       def address;    @options[:address]  end
       def socket;     @options[:socket]   end
@@ -33,35 +33,35 @@ module Thin
       def only;       @options[:only]     end
       def onebyone;   @options[:onebyone] end
       def wait;       @options[:wait]     end
-      
+
       def swiftiply?
         @options.has_key?(:swiftiply)
       end
-    
+
       # Start the servers
       def start
         with_each_server { |n| start_server n }
       end
-    
+
       # Start a single server
       def start_server(number)
         log_info "Starting server on #{server_id(number)} ... "
-      
+
         run :start, number
       end
-  
+
       # Stop the servers
       def stop
         with_each_server { |n| stop_server n }
       end
-    
+
       # Stop a single server
       def stop_server(number)
         log_info "Stopping server on #{server_id(number)} ... "
-      
+
         run :stop, number
       end
-    
+
       # Stop and start the servers.
       def restart
         unless onebyone
@@ -70,7 +70,7 @@ module Thin
           sleep 0.1 # Let's breath a bit shall we ?
           start
         else
-          with_each_server do |n| 
+          with_each_server do |n|
             stop_server(n)
             sleep 0.1 # Let's breath a bit shall we ?
             start_server(n)
@@ -78,26 +78,25 @@ module Thin
           end
         end
       end
-      
-      def test_socket(number)
-        if socket
-          UNIXSocket.new(socket_for(number))
-        else
-          TCPSocket.new(address, number)
+
+      def test_server(number)
+        begin
+          response = Net::HTTP.get_response(address, '/', number)
+          return response.is_a?(Net::HTTPSuccess)
+        rescue Errno::ECONNREFUSED => e
+          # the server isn't responsive yet
+          return false
         end
-      rescue
-        nil
       end
-      
+
       # Make sure the server is running before moving on to the next one.
       def wait_until_server_started(number)
         log_info "Waiting for server to start ..."
         STDOUT.flush # Need this to make sure user got the message
-        
+
         tries = 0
         loop do
-          if test_socket = test_socket(number)
-            test_socket.close
+          if test_server(number)
             break
           elsif tries < wait
             sleep 1
@@ -109,7 +108,7 @@ module Thin
           end
         end
       end
-    
+
       def server_id(number)
         if socket
           socket_for(number)
@@ -119,23 +118,23 @@ module Thin
           [address, number].join(':')
         end
       end
-    
+
       def log_file_for(number)
         include_server_number log_file, number
       end
-    
+
       def pid_file_for(number)
         include_server_number pid_file, number
       end
-    
+
       def socket_for(number)
         include_server_number socket, number
       end
-    
+
       def pid_for(number)
         File.read(pid_file_for(number)).chomp.to_i
       end
-      
+
       private
         # Send the command to the +thin+ script
         def run(cmd, number)
@@ -150,7 +149,7 @@ module Thin
           end
           Command.run(cmd, cmd_options)
         end
-      
+
         def with_each_server
           if only
             if first_port && only < 80
@@ -166,7 +165,7 @@ module Thin
             size.times { |n| yield first_port + n }
           end
         end
-      
+
         # Add the server port or number in the filename
         # so each instance get its own file
         def include_server_number(path, number)
